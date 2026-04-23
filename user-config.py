6ff9b30d8e563c71ef5f24d53886515ffe939fa8
Rupert28 <r_morgan12@outlook.com>
2026-04-23 17:55:03 +1000

    fix syntax in config

--- user-config.py
+++ user-config.py
@@ -1,13 +1,15 @@
 import os
-
+from pywikibot.login import BotPassword
 
 family = 'wikipedia'
-
 mylang = 'en'
-
 usernames['wikipedia']['en'] = 'SnowyBot'
 
 password_file = None
 
 def password_callback(shell, username):
-    return os.environ.get('SNOWY_PASS')
+    secret = os.environ.get('SNOWY_PASS')
+    
+    if secret:
+        return BotPassword('Task_1', secret)
+    return None
